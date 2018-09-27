397e1ce4c6985c5846f374bf8d5db07f21941aba
USER <USER@USER-PC>
2018-09-27 12:15:24 +0900

    빌드파일

---
+++ 213/src/d/dfsa.java
@@ -0,0 +1,14 @@
+package d;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class dfsa {
+
+	@Test
+	void test() {
+		assertEquals(3,dd.sum(1, 2));
+	}
+
+}
